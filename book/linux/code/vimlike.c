ec5e4319d689f5250914e156f953245101e1fc13
Alex <hnzhang1@gzu.edu.cn>
2018-05-03 14:40:32 +0800

    vimlike.c
    the 1st edtion.can display a file contents in one screen.

---
+++ book/linux/code/vimlike.c
@@ -0,0 +1,55 @@
+#include<unistd.h>
+#include<curses.h>
+#include<stdlib.h>
+
+void readAfile(char * fileName, WINDOW * win);
+int main(){
+	WINDOW * fwin;
+	WINDOW * swin;
+	char * fileName;
+	fileName = (char *) malloc(32);
+	fwin=initscr();
+//add some colors
+	if(has_colors()){
+		start_color();
+		init_pair(1,COLOR_GREEN,COLOR_BLACK);
+		attron(COLOR_PAIR(1));
+	}
+//give some tips
+	addstr("choose an option:\nO for open a file\nQ for quit");
+//detect the keypress
+	while(1){
+		int key=getch();
+		char key_press=(char)key;
+		switch(key_press){
+			case 'Q':
+				endwin();
+				exit(0);
+			case 'O':
+				swin = subwin(fwin,3,15,20,30);
+				box(swin,'#','#');
+				wmove(swin,1,2);
+				wgetstr(swin,fileName);	
+				wrefresh(swin);
+				readAfile(fileName,fwin);
+				delwin(swin);
+				//waddstr(fwin,fileName);
+
+		}
+	}
+	refresh();
+	sleep(5);
+	free(fileName);
+	endwin();
+	exit(0);
+}
+void readAfile(char * fileName,WINDOW * win){
+	FILE * file = fopen(fileName,"r");
+	int ch= fgetc(file);
+	while(ch!=EOF){
+		waddch(win,(char)ch);
+		wrefresh(win);
+		ch= fgetc(file);
+	}	
+	fclose(file);
+}
